83ae7c4e579234465d413d7927665af0df505126
tomas f <46322567+TomFern@users.noreply.github.com>
2021-04-15 13:28:55 -0300

    add unit tests

--- web/tests/unit/hello.js
+++ web/tests/unit/hello.js
@@ -1,13 +1,12 @@
-const request = require('supertest');
-const { assert } = require('chai');
+// test api endpoint /api/hello
 
-request('http://localhost:3000')
-    .get('/api/hello')
-    .set('accept', 'application/json')
-    .expect('content-type', "application/json; charset=utf-8")
-    .expect(200)
-    .then(response => {
-        assert.deepEqual(JSON.parse(response.text), { name: 'John Doe' });
-    })
-    .catch(console.log);
+require = require("esm")(module);
+const { assert } = require("chai");
+const { testApiHandler } = require('next-test-api-route-handler');
+const handler = require('../../pages/api/hello');
 
+testApiHandler({handler,   test: async ({ fetch }) => {
+    const res = await fetch();
+    const data = await res.json();
+    assert.deepEqual(data, { name: 'John Doe' });
+}});
