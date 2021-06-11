5b3443b7b8f7a42e0abcfc12343992a65492e59e
tomas f <46322567+TomFern@users.noreply.github.com>
2021-06-11 17:18:44 -0300

    u

--- packages/web/apollo-client.js
+++
@@ -1,8 +0,0 @@
-// GraphQL Client
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-const client = new ApolloClient({ 
-    uri: process.env.API_URL || "http://localhost:4000",
-    cache: new InMemoryCache(),
-});
-
-export default client;
